0a54979bf93d4a9c0dac7bff130ab22b36ff30af
fangzhiyang <fanmcgradfy@gmail.com>
2021-02-23 14:17:13 +0800

    修改启动方式

--- my-schedule.py
+++ my-schedule.py
@@ -6,7 +6,7 @@ from jwc import job as job1
 from weifuwu import job as job2
 
 schedule.every(10).minutes.do(job1) # 每10分钟监控教务处新闻
-schedule.every().day.at("14:10").do(job2) # 每天6：30打卡
+schedule.every().day.at("06:30").do(job2) # 每天6：30打卡
 
 while True:
     schedule.run_pending() # 运行所有可运行的任务
